22f086d7d6f42462ef73a84e6b3f75e004c96972
llamda <114190545+llamda@users.noreply.github.com>
2023-01-23 18:51:54 -0500

    Add file hashing

---
+++ src/hash.rs
@@ -0,0 +1,9 @@
+use blake3::{Hash, Hasher};
+use std::{error::Error, fs, io, path::Path};
+
+pub fn hash_file_blake3(path: &Path) -> Result<Hash, Box<dyn Error>> {
+    let mut file = fs::File::open(&path)?;
+    let mut hasher = Hasher::new();
+    io::copy(&mut file, &mut hasher)?;
+    Ok(hasher.finalize())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 mod config;
+mod hash;
 use config::Config;
+use std::{env, path::Path};
 
 fn main() {
     let config = Config::get();
     println!("{:?}", config);
+
+    // Test Hashing
+    let arg = env::args().nth(1).unwrap();
+    let result = hash::hash_file_blake3(Path::new(&arg));
+    println!("{:?}", result);
 }
