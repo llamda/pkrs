3af91ad4b1a278c2bb04619b38cd2872edf40188
llamda <114190545+llamda@users.noreply.github.com>
2023-01-27 17:41:22 -0500

    DB hash storage

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,4 @@
-use rusqlite::Connection;
+use rusqlite::{Connection, Error, Result};
 
 #[derive(Debug)]
 pub struct Database {
@@ -6,8 +6,31 @@ pub struct Database {
 }
 
 impl Database {
-    pub fn connect(path: String) -> Self {
+    pub fn connect(path: &String) -> Self {
         let conn = Connection::open(path).expect("Failed to open the sqlite database?");
-        Database { conn }
+        let db = Database { conn };
+
+        db.create_tables().expect("Failed to create a table?");
+        db
+    }
+
+    fn create_tables(&self) -> Result<()> {
+        self.conn.execute(
+            "CREATE TABLE IF NOT EXISTS posts (
+            postId INTEGER PRIMARY KEY,
+            blake3 BLOB NOT NULL UNIQUE
+        )",
+            (),
+        )?;
+        Ok(())
+    }
+
+    pub fn insert_post(&mut self, hash: &[u8]) -> Result<i64, Error> {
+        let mut stmt = self
+            .conn
+            .prepare_cached("INSERT OR IGNORE INTO posts (blake3) VALUES (?1)")?;
+
+        stmt.execute(&[hash])?;
+        Ok(self.conn.last_insert_rowid())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,21 @@ mod db;
 mod hash;
 use config::Config;
 use db::Database;
+use std::{env, path::Path};
+
 fn main() {
     let config = Config::get();
 
     println!("{:?}", config);
 
-    let db = Database::connect(config.db_sql_path);
+    let mut db = Database::connect(&config.db_sql_path);
     println!("{:?}", db);
+
+    // Test hash storage
+    let arg = env::args().nth(1).unwrap();
+    let result = hash::hash_file_blake3(Path::new(&arg));
+    println!("{:?}", result);
+
+    let result = db.insert_post(result.unwrap().as_bytes());
+    println!("{:?}", result);
 }
