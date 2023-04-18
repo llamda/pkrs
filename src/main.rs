976dc10eb4d2cb197243affc24cb8dc1fc2c293e
llamda <114190545+llamda@users.noreply.github.com>
2023-04-17 20:14:03 -0400

    Open GUI when no args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 mod cli;
 mod config;
 mod db;
@@ -10,12 +11,18 @@ mod worker;
 use cli::Cli;
 use config::Config;
 use db::Database;
+use std::env;
 
 fn main() {
     let config = Config::get();
     let db = Database::connect(config);
 
-    if let Err(e) = Cli::run(db) {
+    let run_result = match env::args().len() < 2 {
+        true => gui::run(db).map_err(|e| e.into()),
+        false => Cli::run(db),
+    };
+
+    if let Err(e) = run_result {
         eprintln!("{:#?}", e);
     }
 }
